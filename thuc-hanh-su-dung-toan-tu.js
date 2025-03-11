0154ea092ec8e9840b567d3125b7768441677645
hoangtuanc02 <tuan4695hatinh@gmail.com>
2025-03-11 15:02:15 +0700

    .

---
+++ thuc-hanh-su-dung-toan-tu.js
@@ -0,0 +1,4 @@
+let width = parseInt(prompt('Nhập chiều rộng nhé bạn yêu ơi!'))
+let length = parseInt(prompt('Nhập chiều dài đi bạn!'))
+let area = parseInt(width*length)
+alert('Thầy Tuấn đọc đáp án cho em yêu nè: '+area)
